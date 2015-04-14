478b6185ba521598c0166a2315f22134c5d46036
Sneha Reddy <sdevireddy1@student.gsu.edu>
2015-04-13 22:32:56 -0400

    f test interface

---
+++ admproj/src/ICoff_Values.java
@@ -0,0 +1,13 @@
+
+public interface ICoff_Values {
+
+	
+	
+	
+	public int size();
+	
+	public int getindex(int indx);
+
+	 
+	 
+}
--- admproj/src/coff_values.java
+++ admproj/src/coff_values.java
@@ -1,8 +1,27 @@
 import java.util.ArrayList;
 
 
-public class coff_values {
 
-	 int cls_lable;
-	 ArrayList<Double> coff;
+
+public class coff_values implements ICoff_Values{
+
+      int cls_lable;
+	  ArrayList<Double> coff;
+	 
+	
+	 
+	 public coff_values(){
+	 
+	 }
+	 
+	 public int size() {
+			return this.coff.size();
+		}
+	 
+
+	
+	public int getindex(int indx) {
+		  return coff.indexOf(indx);
+		}
+	 
 }
