fbe5138b9214c1272fb68e4decec3c11ce1a4253
Sneha Reddy <sdevireddy1@student.gsu.edu>
2015-04-13 20:24:16 -0400

    Ftest

---
+++ admproj/src/coff_values.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+
+public class coff_values {
+
+	 int cls_lable;
+	 ArrayList<Double> coff;
+}
---
+++ admproj/src/fvalues.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+
+public interface fvalues {
+ 
+	public ArrayList<Double> ftestcalc_updated(ArrayList<coff_values> coll);
+}
